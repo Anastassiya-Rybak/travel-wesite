0ce083d119d3158dfd2025c523ccc89f97e41361
Obliviate <vyacheslavovna.an@mail.ru>
2023-12-10 00:28:34 +0600

    feat: adaptive for 2 block

--- js/slider.js
+++ js/slider.js
@@ -10,8 +10,20 @@ $('.slider2').slick({
     slidesToShow: 3,
     slidesToScroll: 3,
     autoplay: true,
-    autoplayspeed: 3000,
+    autoplayspeed: 5000,
     pauseOnHover: true,
     pauseOnDotsHover: true
     // variableWidth: true
+});
+
+$('.slider3').slick({
+  lazyLoad: 'ondemand',
+  dots: true,
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  autoplay: true,
+  autoplayspeed: 5000,
+  pauseOnHover: true,
+  pauseOnDotsHover: true
+  // variableWidth: true
 });
