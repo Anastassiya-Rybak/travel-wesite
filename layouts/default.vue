0e2c3aa1b414ba24e2227e3e0adf8d70a48d36d6
Obliviate <vyacheslavovna.an@mail.ru>
2023-12-11 18:30:02 +0600

    fix: migration header with headers popups

--- layouts/default.vue
+++ layouts/default.vue
@@ -25,11 +25,14 @@
             <section class="header__white-back-wrapper">
                 <div class="header__white-back wrapper">
                     <div class="header__logo"><a href=""><img src="/images/logo.webp" alt="Lime Trip"></a></div>
-                    <h3 class="header__burger" data-modal-open="burgernav">МЕНЮ</h3>
+                    <div class="header__burger-wrapper">
+                        <h3 class="header__burger" @click.prevent="openPopup('burger')">МЕНЮ</h3>
+                        <BurgerContent :open="open.burger" />
+                    </div>
                     <ul class="header__nav">
                         <nuxt-link to="">Главная</nuxt-link>
-                        <nuxt-link to="" data-anchor="destinationnav">О направлениях</nuxt-link>
-                        <nuxt-link to="blogs">Блоги</nuxt-link>
+                        <nuxt-link to="" @click.prevent="openPopup('destinations')">О направлениях</nuxt-link>
+                        <nuxt-link to="">Блоги</nuxt-link>
                         <nuxt-link to="">Контакты</nuxt-link>
                         <nuxt-link to="">Новости</nuxt-link>
                     </ul>
@@ -37,34 +40,48 @@
                         <div class="header__search">
                             <img src="/images/icons/search.svg" alt="Поиск">
                         </div>
-                        <div class="header__user-icon" data-modal-open="usersignin">
-                            <img src="/images/icons/user.svg" alt="">
+                        <div class="header__user-icon" @click.prevent="openPopup('modal')" >
+                            <img src="/images/icons/user.svg" alt="Войти/Зврегистрироваться">
                         </div>
                         <nuxtLink class="header__save" to="">
                             <img src="/images/icons/save.svg" alt="Сохранённое">
                         </nuxtLink>
                     </div>
-                </div> 
+                </div>
             </section>
+            <TheDistanations :open="open.destinations" />
         </header>
+        <ModalWindow v-show="open.modal" type="usersProfile" @close-modal="open.modal = false" />
         <slot />
     </div>
 </template>
 
 <script>
-// import { BurgerContent } from '#build/components'
+import TheDistanations from '~/components/popups/TheDistanations.vue';
+import BurgerContent from '~/components/popups/BurgerContent.vue';
+import ModalWindow from '~/components/popups/ModalWindow.vue';
 
 export default {
-    // components: {BurgerContent},
-    setup () {
-        
-
-        return {}
+    components: { TheDistanations, BurgerContent, ModalWindow },
+    data() {
+        return {
+            open: {
+                destinations: false,
+                burger: false,
+                modal: false
+            }
+        }
+    },
+    methods: {
+        openPopup(dir){
+            this.open[dir] === true ? this.open[dir] = false : this.open[dir] = true;
+        }
     }
+           
 }
 </script>
 
-<style lang="scss" scoped>
+<style lang="scss">
     .header {
         display: flex;
         flex-direction: column;
@@ -80,11 +97,21 @@ export default {
             justify-content: space-between;
             align-items: center;
             padding: 15px 0;
+
+            @include media(560px) {
+                flex-wrap: wrap;
+                row-gap: 10px;
+                justify-content: space-between;
+            }
         }
 
         &__info {
             display: flex;
             align-items: center;
+            @include media(560px) {
+                width: 70%;
+                margin: 0 auto;
+            }
 
             .info__number {
                 display: flex;
@@ -95,6 +122,12 @@ export default {
                 a {
                     color: #fff;
                     font-size: 14px;
+                    @include media(680px) {
+                        font-size: 10px;
+                    }
+                    @include media(560px) {
+                        font-size: 8px;
+                    }
 
                     &:hover {
                         @include link-hover;
@@ -104,6 +137,9 @@ export default {
                 img {
                     filter: invert(1);
                     width: 20px;
+                    @include media(680px) {
+                        width: 15px;
+                    }
                 }
             }
 
@@ -111,6 +147,13 @@ export default {
                 font-weight: 600;
                 opacity: 0.8;
                 font-size: 14px;
+                @include media(680px) {
+                    font-size: 8px;
+                }
+                @include media(560px) {
+                    font-size: 8px;
+                    letter-spacing: 0.1em;
+                }
                 letter-spacing: 0.3em;
             }
         }
@@ -119,9 +162,16 @@ export default {
             display: flex;
             justify-content: space-between;
             width: 15%;
+            @include media(560px) {
+                width: 70%;
+                margin: 0 auto;
+            }
 
             a {
                 width: calc(23% - 10%);
+                @include media(560px) {
+                    width: 7%;
+                }
 
                 img {
                     filter: invert(1);
@@ -146,11 +196,22 @@ export default {
             justify-content: space-between;
             align-items: center;
             padding: 40px 0;
-            width: 100%;
+            @include media(1100px) {
+                padding: 20px 0;
+            }
         }
 
         &__logo {
             width: 13%;
+            @include media(880px) {
+                width: 20%;
+            }
+            @include media(680px) {
+                width: 10%;
+            }
+            @include media(560px) {
+                width: 25%;
+            }
 
             a {
                 width: 100%;
@@ -161,10 +222,17 @@ export default {
             }
         }
 
-        &__burger {
+        &__burger-wrapper {
             width: 50%;
             position: relative;
             display: none;
+            @include media(560px) {
+                display: block;
+            }
+        }
+
+        &__burger {
+            width: 100%;
             text-align: center;
             color: #b2e400;
             font-size: 16px;
@@ -182,7 +250,16 @@ export default {
 
         &__nav {
             display: flex;
+            @include media(560px) {
+                display: none;
+            }
             width: 50%;
+            @include media(1100px) {
+                width: 60%;
+            }
+            @include media(680px) {
+                width: 70%;
+            }
             justify-content: space-between;
             align-items: center;
 
@@ -191,6 +268,9 @@ export default {
                 font-weight: 500;
                 text-transform: uppercase;
                 font-size: 14px;
+                @include media(880px) {
+                    font-size: 10px;
+                }
 
                 &:hover {
                     @include link-hover;
@@ -203,6 +283,12 @@ export default {
             justify-content: space-between;
             align-items: center;
             width: 12%;
+            @include media(1100px) {
+                width: 10%;
+            }
+            @include media(560px) {
+                width: 18%;
+            }
         }
 
         &__search,
@@ -210,12 +296,13 @@ export default {
         &__save {
             width: calc(18% - 3%);
             cursor: pointer;
+            @include media(560px) {
+                width: calc(25% - 3%);
+            }
 
             &:hover {
                 transform: scale(1.07);
             }
         }
-
-
     }
 </style>
